6bb8582ccb9b6981ad3e8c4b8132ba7934534c9e
Nivin34 <nivin33456@gmail.com>
2025-03-25 11:15:03 +0530

    second commits

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const productRoutes = require("./routes/productRoutes");
 
 const cors = require("cors");
 const app = express();
-dotenv.config({path: path.join(__dirname, 'config', '.env')})
+dotenv.config({path: path.join(__dirname,'.env')})
 
 // Middleware
 app.use(bodyParser.json());
